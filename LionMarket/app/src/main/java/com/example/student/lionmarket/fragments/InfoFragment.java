89fe86b856fa15c21a10eafb0f89b219a7244313
Levon <40332248+levons2000@users.noreply.github.com>
2018-07-24 17:24:51 +0400

    Add files via upload

---
+++ LionMarket/app/src/main/java/com/example/student/lionmarket/fragments/InfoFragment.java
@@ -0,0 +1,68 @@
+package com.example.student.lionmarket.fragments;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.example.student.lionmarket.R;
+import com.example.student.lionmarket.models.GameModel;
+import com.example.student.lionmarket.providers.AllItemDatas;
+import com.squareup.picasso.Picasso;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InfoFragment extends android.app.Fragment {
+
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        ImageView imageView = view.findViewById(R.id.info_image);
+        TextView textName = view.findViewById(R.id.info_name);
+        TextView textType = view.findViewById(R.id.info_type);
+        TextView textPrice = view.findViewById(R.id.info_price);
+        TextView textCreator = view.findViewById(R.id.info_creator);
+        RatingBar ratingBar = view.findViewById(R.id.ratingBar);
+        GameModel gameModel = AllItemDatas.list.get(AllItemDatas.position);
+        Picasso.get().load(gameModel.getImgUrl()[0]).into(imageView);
+        textName.setText(gameModel.getName());
+        textType.setText(gameModel.getType());
+        textPrice.setText(gameModel.getPrice());
+        textCreator.setText(gameModel.getCreator());
+        ratingBar.setRating(gameModel.getRaiting());
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                AllItemDatas.list.get(AllItemDatas.position).setRaiting(v);
+            }
+        });
+        setVideoUrl(AllItemDatas.list.get(AllItemDatas.position).getVideoLink(), view);
+        TextView textDesc = view.findViewById(R.id.desc);
+        textDesc.setText(AllItemDatas.list.get(AllItemDatas.position).getDesc());
+
+        return view;
+    }
+
+    private void setVideoUrl(String url, View view){
+        VideoView videoView = view.findViewById(R.id.video);
+        videoView.setVideoURI(Uri.parse(url));
+        videoView.start();
+    }
+
+}
